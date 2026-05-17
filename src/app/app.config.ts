0ab068066d70fb099ea7767658af7c787c9eb9d2
Dmitriy <john.smith.dm8338@gmail.com>
2026-05-17 14:58:53 +0300

    - hash location added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,11 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter, withViewTransitions } from '@angular/router';
+import { provideRouter, withHashLocation, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes, withViewTransitions()),
+    provideRouter(routes, withViewTransitions(), withHashLocation()),
   ]
 };
